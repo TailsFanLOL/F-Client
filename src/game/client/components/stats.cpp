1750b57b0179cba8ce2fbb88996d9a17aac071b8
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2024-07-02 18:26:06 +0200

    fix warning

--- src/game/client/components/stats.cpp
+++ src/game/client/components/stats.cpp
@@ -451,14 +451,14 @@ void CStats::OnRender()
 			const float RoundSize = BarHeight/2.0f;
 			float EndX = StartX; // each bar will have its width incremented by the roundsize so this avoids that last one would overflow
 			int TotalFrags = 0;
-			int TotalDeaths = 0;
+			//int TotalDeaths = 0;
 			for(i=0; i<NUM_WEAPONS; i++)
 			{
 				if(aDisplayWeapon[i])
 				{
 					EndX += 80.0f;
 					TotalFrags += pStats->m_aFragsWith[i];
-					TotalDeaths += pStats->m_aDeathsFrom[i];
+					//TotalDeaths += pStats->m_aDeathsFrom[i];
 				}					
 			}
 			float ExploitableLength = (EndX-StartX) - RoundSize;
